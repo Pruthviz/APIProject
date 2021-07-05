6c3d7fa38dabd5c47ef6f0ec68f543412dc87ce8
Pruthviraj SP <Prudodo@gmail.com>
2021-07-05 08:10:36 +0530

    Added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# APIProject
+# spring-boot-rest-crud
+Building Spring Boot 2.X RESTful CRUD API with Spring Data JPA, Hibernate, Lombok and MySQL in 7 simple steps
---
+++ src/main/java/com/api/product/entity/Product.java
@@ -0,0 +1,54 @@
+package com.api.product.entity;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String price;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, price);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name) && Objects.equals(price, other.price);
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}	
+}
